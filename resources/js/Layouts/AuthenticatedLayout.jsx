4389e1b6fe0fba637991dc59db2334929b3af43b
pritom06 <pritomsaha20022018@gmail.com>
2026-02-11 18:44:49 +0530

    login logout worked fine

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -3,14 +3,18 @@ import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link, usePage } from '@inertiajs/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function AuthenticatedLayout({ header, children }) {
-    const user = usePage().props.auth.user;
+    const page = usePage();
+    const user = page.props.auth.user;
 
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
 
+    useEffect(() => {
+        console.log("AuthenticatedLayout Mounted.");
+    }, []);
     return (
         <div className="min-h-screen bg-gray-100">
             <nav className="border-b border-gray-100 bg-white">
---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import ChatLayout from '@/Layouts/ChatLayout';
+
+export default function Home({ auth }) {
+    return (
+        <ChatLayout>
+            Messages
+        </ChatLayout>
+    );
+}
