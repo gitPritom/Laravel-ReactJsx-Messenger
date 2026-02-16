06c5d461674f6940bc88c857e304ee06609f47d1
pritom06 <pritomsaha20022018@gmail.com>
2026-02-16 18:45:09 +0530

    Message are showing

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -5,36 +5,38 @@ import ChatLayout from '@/Layouts/ChatLayout';
 import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid';
 import { useEffect, useRef, useState } from 'react';
 
-function Home({ selectedConversation = null, messages = null,  }) {
+function Home({ selectedConversation = null, messages = null }) {
+    // console.log("Messages: ",messages);
     const [localMessages, setLocalMessages] = useState([]);
     const messagesCtrRef = useRef(null);
 
     useEffect(() => {
-        setLocalMessages(messages);
+        setLocalMessages(messages ? messages.data.reverse() : []);
     }, [messages]);
 
     return (
         <>
             {!messages && (
                 <div className="flex flex-col gap-8 text-center opacity-35 items-center justify-center h-full">
-                    <div className="text-gray-500 text-2xl md:text-4xl p-16 text-slate-200">Select a conversation to start chatting</div>
+                    <div className="text-gray-500 text-2xl md:text-4xl p-16 text-slate-200">
+                        Select a conversation to start chatting
+                    </div>
                     <ChatBubbleLeftRightIcon className="w-32 h-32 inline-block" />
                 </div>
             )}
-            {
-                messages && (<>
-                    <ConversationHeader selectedConversation={selectedConversation} />
-                    <div ref={messagesCtrRef} className='flex-1 overflow-y-auto p-5'>
-                        {/* Messages */}
-                        {localMessages.length === 0 && (
-                            <div className="flex flex-col flex ">
-                                {localMessages.map((message) => (
-                                    <MessageItem key={message.id} message={message} />
-                                ))}
-                            </div>)}
-                    </div>
-                    {/* <MessageInput conversation={selectedConversation} /> */}
-                </>)
+            {messages && (<>
+                <ConversationHeader selectedConversation={selectedConversation} />
+                <div ref={messagesCtrRef} className='flex-1 overflow-y-auto p-5'>
+                    {/* Messages */}
+                    {localMessages.length === 0 && (
+                        <div className="flex flex-col flex ">
+                            {localMessages.map((message) => (
+                                <MessageItem key={message.id} message={message} />
+                            ))}
+                        </div>)}
+                </div>
+                {/* <MessageInput conversation={selectedConversation} /> */}
+            </>)
             }
         </>
     );
