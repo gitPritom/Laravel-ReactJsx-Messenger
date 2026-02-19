839540135f662f49f421d1e4fdeb2ab7cbf35d83
pritom06 <pritomsaha20022018@gmail.com>
2026-02-19 12:32:47 +0530

    mesasge date and current in the top in side pannel

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,5 +1,6 @@
 import ConversationItem from "@/Components/App/conversationItem";
 import TextInput from "@/Components/TextInput";
+import { useEventBus } from "@/EventBus";
 import { PencilSquareIcon } from "@heroicons/react/24/solid";
 import { usePage } from "@inertiajs/react";
 import { use, useEffect, useState } from "react";
@@ -11,12 +12,13 @@ const ChatLayout = ({ children }) => {
     const [localConversations, setLocalConversations] = useState([]);
     const [sortedConversations, setSortedConversations] = useState([]);
     const [onlineUsers, setOnlineUsers] = useState({});
+    const { on } = useEventBus();
 
     const isUserOnline = (userId) => {
         if (!onlineUsers || typeof onlineUsers !== 'object') return false;
         return !!onlineUsers[userId];
     };
-    
+
     const onSearch = (e) => {
         const searchTerm = e.target.value.toLowerCase();
         setLocalConversations(
@@ -27,6 +29,43 @@ const ChatLayout = ({ children }) => {
             }),
         );
     }
+
+    const messageCreated = (message) => {
+        setLocalConversations((oldUsers) => {
+            return oldUsers.map((u) => {
+                //If the mesage is for user
+                if (
+                    message.receiver_id &&
+                    !u.is_group &&
+                    (u.id == message.sender_id ||
+                        u.id == message.receiver_id)
+                ) {
+                    u.last_message = message.message;
+                    u.last_message_date = message.created_at;
+                    return u;
+                }
+                //If the message is for group
+                if (
+                    message.group_id &&
+                    u.is_group &&
+                    u.id == message.group_id
+                ) {
+                    u.last_message = message.message;
+                    u.last_message_date = message.created_at;
+                    return u;
+                }
+
+                return u;
+            });
+        });
+    };
+
+    useEffect(() => {
+        const offCreated = on("message.created", messageCreated);
+        return () => {
+            offCreated();
+        };
+    }, [on]);
 
     useEffect(() => {
         setSortedConversations(
