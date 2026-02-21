ac6597b3bfbf842d9eb6ba3f2cfb3024d1cdd82f
pritom06 <pritomsaha20022018@gmail.com>
2026-02-21 08:41:45 +0530

    Commmet something

--- resources/js/Components/App/CustomAudioPlayer.jsx
+++ resources/js/Components/App/CustomAudioPlayer.jsx
@@ -2,6 +2,7 @@ import { PauseCircleIcon, PlayCircleIcon } from "@heroicons/react/24/solid";
 import { useRef, useState } from "react";
 
 
+//Custom Audio Player
 const CustomAudioPlayer = ({ file, showVolume = true }) => {
     const audioRef = useRef();
     const [isPlaying, setIsPlaying] = useState(false);
